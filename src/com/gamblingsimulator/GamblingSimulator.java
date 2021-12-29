79103a0ec2c83779dcd02c99ce83e6e757047294
NayanTr09 <nayantripathi42@gmail.com>
2021-12-29 08:58:54 +0530

    Modified : Gitignore file

--- src/com/gamblingsimulator/GamblingSimulator.java
+++ src/com/gamblingsimulator/GamblingSimulator.java
@@ -10,6 +10,6 @@ public class GamblingSimulator {
     public static void main(String args[]){
         System.out.println("Welcome to Gambling Simulator!");
         int Stake = 100;  // Initialized Stake
-        int bet = 1;   // Inititialized Bet
+        int bet = 1;   // Initialized Bet
     }
 }
