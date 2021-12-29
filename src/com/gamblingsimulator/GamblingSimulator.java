a0196b9064990c5fae8167723ecf278e13256fa4
NayanTr09 <nayantripathi42@gmail.com>
2021-12-29 09:21:01 +0530

    Added : Check Win or Lose Functionality

--- src/com/gamblingsimulator/GamblingSimulator.java
+++ src/com/gamblingsimulator/GamblingSimulator.java
@@ -6,10 +6,25 @@
 
 package com.gamblingsimulator;
 
+import java.util.Random;
+
 public class GamblingSimulator {
     public static void main(String args[]){
         System.out.println("Welcome to Gambling Simulator!");
         int Stake = 100;  // Initialized Stake
         int bet = 1;   // Initialized Bet
+        WinorLose();
+    }
+
+    public static int WinorLose(){
+        Random Ran = new Random();
+        int StartBet = Ran.nextInt(2);
+        if(StartBet == 1) {
+            System.out.println("Gambler wins the bet");
+        }
+            else {
+            System.out.println("Gambler looses the bet");
+            }
+        return WinorLose();
     }
 }
