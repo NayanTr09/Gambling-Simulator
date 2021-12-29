17fbb2ed4007e8aad88eaf3d6b4023bebebb1b65
NayanTr09 <nayantripathi42@gmail.com>
2021-12-29 08:41:30 +0530

    Initialized variables : Stake and Bet

--- src/com/gamblingsimulator/GamblingSimulator.java
+++ src/com/gamblingsimulator/GamblingSimulator.java
@@ -1,8 +1,15 @@
+/**
+ *  Workshop : Gambling Simulator Program
+ * @author Nayan Tripathi
+ * Date : 29th December, 2021
+ */
+
 package com.gamblingsimulator;
 
 public class GamblingSimulator {
     public static void main(String args[]){
         System.out.println("Welcome to Gambling Simulator!");
-        int Stake= 100;
+        int Stake = 100;  // Initialized Stake
+        int bet = 1;   // Inititialized Bet
     }
 }
